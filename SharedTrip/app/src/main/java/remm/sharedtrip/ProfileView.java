ba49d6481d5e9e62c98f8c7bcbb4e4457a492c53
markhenrip <markhenripedoson@gmail.com>
2017-10-22 21:21:41 +0300

    #25 Created files for Profile view

---
+++ SharedTrip/app/src/main/java/remm/sharedtrip/ProfileView.java
@@ -0,0 +1,14 @@
+package remm.sharedtrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class ProfileView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
