b04d6e9e13f2d3144d2fc779c4fd2e3cd6fa995b
markhenrip <markhenripedoson@gmail.com>
2017-10-24 22:27:36 +0300

    #12 Added Event creation floating action button

--- SharedTrip/app/src/main/java/remm/sharedtrip/BrowseEvents.java
+++ SharedTrip/app/src/main/java/remm/sharedtrip/BrowseEvents.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+
 
 public class BrowseEvents extends AppCompatActivity {
 
@@ -18,13 +21,14 @@ public class BrowseEvents extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_browse_events);
 
-        Button add_event_button = (Button) findViewById(R.id.add_event_button);
-        add_event_button.setOnClickListener(new View.OnClickListener(){
-
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_event_fbutton);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Intent myIntent = new Intent(BrowseEvents.this, CreateEvent.class);
                 BrowseEvents.this.startActivity(myIntent);
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
     }
