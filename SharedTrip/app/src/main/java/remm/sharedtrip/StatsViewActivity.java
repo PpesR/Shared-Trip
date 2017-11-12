8d9d08872871f98571c6f9be29a225a1b8cc980c
R Sepp <robietoser@hotmail.com>
2017-11-12 20:00:18 +0200

    Added coming soon to stats page #34

--- SharedTrip/app/src/main/java/remm/sharedtrip/StatsViewActivity.java
+++ SharedTrip/app/src/main/java/remm/sharedtrip/StatsViewActivity.java
@@ -1,13 +1,53 @@
 package remm.sharedtrip;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class StatsViewActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stats_view_activity);
+
+        setContentView(R.layout.activity_stats_view);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        BottomNavigationViewHelper
+                .disableShiftMode(bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.bottombaritem_events:
+                                finish();
+                                return true;
+                            case R.id.bottombaritem_friends:
+                                Intent friendsViewActivity = new Intent(StatsViewActivity.this, FriendsViewActivity.class);
+                                startActivity(friendsViewActivity);
+                                return true;
+                            case R.id.bottombaritem_stats:
+                                return true;
+                            case R.id.bottombaritem_profile:
+                                // TODO
+                                return true;
+                        }
+                        return true;
+                    }
+                });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MenuItem myButton = bottomNavigationView.getMenu()
+                .findItem(R.id.bottombaritem_stats);
+        myButton.setChecked(true);
     }
 }
